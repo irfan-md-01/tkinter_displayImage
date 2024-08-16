065588d8134cc138451ae080d352e8820525ba9d
Irfan MD <145257683+irfan-md-01@users.noreply.github.com>
2024-08-16 09:27:03 +0530

    Update practice_infoDisplay.py & used for loop, lists to display images

--- practice_infoDisplay.py
+++ practice_infoDisplay.py
@@ -13,37 +13,34 @@ root.geometry("1080x600")
 root.title("My InfoGUI --From tkinter_practise")
 
 lable = Label(text="According to our sources we came to know that --> ", anchor="center", bg="lightblue", font=("Arial", 16, "bold") ,height=2 )
-lable.pack(fill=X,pady=15)
+lable.grid(row=0,column=0,columnspan=4)
 
+list_images=[]
+list_texts=[]
 
-row_frame = Frame(root)
-photo = Image.open("2.jpg")
-img = ImageTk.PhotoImage(photo.resize((150, 150)))
-image_frame = Frame(row_frame)
-label = Label(image_frame, image=img )
-label.pack()
-image_frame.pack()
+for i in range(2, 6):
+    list_texts.append(data(f"{i}.txt"))
+    image_path=None
+    if(os.path.exists(f"{i}.jpg")):
+        image_path=f"{i}.jpg"
+    elif(os.path.exists(f"{i}.webp")):
+        image_path=f"{i}.webp"
+    image = Image.open(image_path)
+    image = image.resize((150, 150))
+    list_images.append(ImageTk.PhotoImage(image))
 
-description_frame = Frame(row_frame)
-description_frame.pack()
-description_label = Label(description_frame, text=data("2.txt"),justify=CENTER, wraplength=500 )
-description_label.pack(padx=10)
-row_frame.pack(pady=15)
-# ----------------------------------------
+for i in range(len(list_images)):
+    if(i==3):
+        row_frame = Frame(root)
+        Label(row_frame, image=list_images[i] ).pack()
+        Label(row_frame, text=list_texts[i], justify=CENTER, wraplength=400 ).pack()
+        row_frame.grid(row=1, column=1, sticky="ne", padx=15, pady=15)
+    else:
+        row_frame = Frame(root)
+        Label(row_frame, image=list_images[i] ).pack()
+        Label(row_frame, text=list_texts[i], justify=CENTER, wraplength=400 ).pack(padx=10)
+        row_frame.grid(row=i+1, column=0, sticky="w", padx=20, pady=15)
 
-row_frame = Frame(root)
-photo = Image.open("3.jpg")
-img3 = ImageTk.PhotoImage(photo.resize((150, 150)))
-image_frame = Frame(root)
-image_frame.pack(padx=10)
-label = Label(image_frame, image=img3 )
-label.pack()
-
-description_frame = Frame(root)
-description_frame.pack()
-description_label = Label(description_frame, text=data("3.txt"),justify=CENTER, wraplength=500 )
-description_label.pack(padx=15)
-row_frame.pack(pady=10)
 
 root.mainloop()
 
